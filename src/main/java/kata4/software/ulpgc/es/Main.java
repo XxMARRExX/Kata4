10ca776f62760973d6adcea67d034742357965a8
XxMARRExX <xx_marre_xx@hotmail.com>
2024-06-29 11:35:18 +0100

    Implementation Main

--- src/main/java/kata4/software/ulpgc/es/Main.java
+++ src/main/java/kata4/software/ulpgc/es/Main.java
@@ -1,10 +1,15 @@
 package kata4.software.ulpgc.es;
 
+import db.kata4.software.ulpgc.es.DataExtractor;
+import db.kata4.software.ulpgc.es.DataInserter;
+import db.kata4.software.ulpgc.es.DatabaseConnector;
+import db.kata4.software.ulpgc.es.TableManagerF1Championship;
 import model.kata4.software.ulpgc.es.*;
 import statistic.kata4.software.ulpgc.es.TeamPointsStatistic;
 import view.kata4.software.ulpgc.es.MainFrame;
 
 import java.io.File;
+import java.sql.Connection;
 import java.util.List;
 import java.util.Map;
 
@@ -15,7 +20,12 @@ public class Main {
                 .with(new File("drivers_updated.csv"))
                 .load();
 
-
+        Connection connection = DatabaseConnector.connect();
+        if(connection != null){
+            TableManagerF1Championship.createTable(connection);
+            new DataInserter().insertDirverRegisters(registers, connection);
+            registers = new DataExtractor().extractF1ChampionshipRecords(connection);
+        }
 
         TeamPointsStatistic statistic = new TeamPointsStatistic();
         Map<String, Float> map = statistic.calculate(registers);
